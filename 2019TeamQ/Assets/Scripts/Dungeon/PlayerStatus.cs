5777a10d1a1cda6af37a72c47aa82dc91c2af735
yukimiran <52032552+yukimiran@users.noreply.github.com>
2019-11-12 13:59:34 +0900

    アイテム表示のプロトタイプver.1.0
    アイテムをQキーで表示します。同時に、時間も止まります。表示するだけだし、リファクタリングもしてないです。

--- 2019TeamQ/Assets/Scripts/Dungeon/PlayerStatus.cs
+++ 2019TeamQ/Assets/Scripts/Dungeon/PlayerStatus.cs
@@ -15,7 +15,41 @@ public class PlayerStatus : MonoBehaviour
     
     
     int floorlevel;
-    
+
+
+
+ //ここからアイテム管理導入
+
+//　アイテムを持っているかどうかのフラグ
+private Dictionary<string, bool> itemFlags = new Dictionary<string, bool>();
+    //　アイテムデータベース
+    [SerializeField]
+    private ItemDataBase itemDataBase;
+
+    private void Start()
+    {
+        //　とりあえず全てのアイテムのフラグを作成
+        foreach (var item in itemDataBase.GetItemDataList())
+        {
+            itemFlags.Add(item.GetItemName(), false);
+        }
+        //　とりあえず適当にアイテムを持っていることにする
+        itemFlags["FlashLight"] = true;
+        itemFlags["BroadSword"] = true;
+        itemFlags["HandGun"] = true;
+    }
+
+    //　アイテムを所持しているかどうか
+    public bool GetItemFlag(string itemName)
+    {
+        return itemFlags[itemName];
+    }
+
+    //ここまでアイテム管理終了。失礼しました
+
+
+
+
     //経験値を得たとき
     public void GetExperience(int exp)
     {
