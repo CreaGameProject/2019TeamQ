e5d452897f61a8f067ad1601a68ffa9575434eb4
yukimiran <52032552+yukimiran@users.noreply.github.com>
2019-12-30 17:44:11 +0900

    アイテム装備
    アイテムを装備できるようになりました。具体的には、アイテムクリック時、装備E表示,アイテムの攻撃力分プレイヤーの攻撃力が上がるようになりました。

--- 2019TeamQ/Assets/Scripts/Item/EquipSlot.cs
+++ 2019TeamQ/Assets/Scripts/Item/EquipSlot.cs
@@ -7,21 +7,51 @@ using UnityEngine.EventSystems;
 public class EquipSlot : MonoBehaviour
 
 {
-    public GameObject Equip_Text;
-    public void OnSortiePointClick(BaseEventData data)
+
+    public ItemData WeaponState;//ItemDataで定義
+    public GameObject EquipText; //equipテキストを取得
+    [SerializeField]
+     private ProcessingSlot processingSlot;//ProcessingSlotスクリプトを取得
+    GameObject ObjectB;//オブジェクト変数を定義
+    PlayerPurameter codeB;//PlayerPuramaterスクリプトを取得
+
+    int WAtk;//武器攻撃力
+
+
+    void Start()
     {
-        var eventData = (PointerEventData)data;
-        //処理...
-        if (Equip_Text.activeSelf == true)
-        {
+        ObjectB = GameObject.Find("GameManager");//Gamemanagerオブジェクトを取得
+        codeB = ObjectB.GetComponent< PlayerPurameter >();//PlayerPurameterを取得
 
-            Equip_Text.SetActive(false);
-        }else if (Equip_Text.activeSelf == false)
-        {
-
-            Equip_Text.SetActive(true);
-        }
     }
-    
-    
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //マウスがクリックされたら
+        {
+ 
+                     WeaponState = GetComponent<ProcessingSlot>().myItemData; //スロットにあるアイテムの名前を取得し変数に代入
+
+            if (codeB.CurrentWeaponState == WeaponState)//currentweaponにスロットにあるアイテムが装備してある場合
+            {
+                codeB.PAtkDown(WAtk);
+                codeB.CurrentWeaponState = codeB.itemDataList[0];//素手をcurrentweaponに装備
+                EquipText.SetActive(false);//装備テキストを解除
+
+            }else if(codeB.CurrentWeaponState != WeaponState)//currentweaponにスロットにあるアイテムが装備してある場合
+
+            {
+            
+                codeB.CurrentWeaponState = WeaponState; //currentweaponにスロットにあるアイテムを装備する
+                EquipText.SetActive(true);    //装備テキストを挿入
+                WAtk = codeB.CurrentWeaponState.GetItemPower();
+                codeB.PAtkUp(WAtk);
+                Debug.Log("攻撃力"+codeB.PAtk);
+            }
+               
+        }
+         
+    }
+        
 }
+
